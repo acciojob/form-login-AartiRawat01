5531f7e0ae36014b297ca9e9be852f9e33e251ec
Aarti Rawat <122513928+AartiRawat01@users.noreply.github.com>
2023-04-13 12:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	 var x=document.getElementById("form1");
+  for (var i=0;i<x.length;i++)
+  {
+   if (x.elements[i].value!='Submit')
+    {  
+      console.log(x.elements[i].value);
+     }  
+   }
 
 }
